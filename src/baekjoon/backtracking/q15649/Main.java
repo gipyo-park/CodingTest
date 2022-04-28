2aa6080586ad059d6c516acb2c52587150de3f71
gipyopark <gipyopark@gmail.com>
2022-04-28 16:33:13 +0900

    [ADD] initial create - 15649 solution

---
+++ src/baekjoon/backtracking/q15649/Main.java
@@ -0,0 +1,60 @@
+package baekjoon.backtracking.q15649;
+
+/**
+ *  N과 M(1)
+ *   - 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+ *   - https://www.acmicpc.net/problem/15649
+ */
+
+import java.io.*;
+
+public class Main {
+    static BufferedReader br;
+    static BufferedWriter bw;
+
+    static int n, m;
+
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void doBackTracking(int depth) throws IOException {
+        if(m == depth) {
+            for(int i=0; i<m; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.newLine();
+
+            return;
+        }
+
+        for(int i=1; i<=n; i++) {
+            if(visited[i]) {
+                continue;
+            }
+
+            visited[i] = true;
+            arr[depth] = i;
+            doBackTracking(depth+1);
+            visited[i] = false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] inStr = br.readLine().split(" ");
+        n = Integer.parseInt(inStr[0]);
+        m = Integer.parseInt(inStr[1]);
+
+        arr = new int[m];
+        visited = new boolean[n+1];
+
+        doBackTracking(0);
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
